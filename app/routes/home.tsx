096026360d24263e9e7049c53b07da4c6bbb0621
AmirK15 <amirkozanov@gmail.com>
2026-01-21 09:51:29 +0600

    fixed import component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { WarehouseManagementSystem } from "../warehouse/warehouse";
+import WarehouseManagementSystem from "../warehouse/warehouse";
 
 export function meta({}: Route.MetaArgs) {
   return [
